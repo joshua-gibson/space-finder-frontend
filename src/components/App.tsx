28c4cd5e35a2a64260b7538e3bf126acdf470771
joshua-gibson <joshua.gibson.dev@gmail.com>
2021-07-15 10:52:55 +0100

    handling events

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,21 @@
-function App() {
-  return <div>app works</div>;
+import React from "react";
+import { User } from "../model/Model";
+import { AuthService } from "../services/AuthService";
+import { Login } from "./Login";
+
+interface AppState {
+  user: User | undefined;
 }
 
-export default App;
+export class App extends React.Component<{}, AppState> {
+  private authService: AuthService = new AuthService();
+
+  render() {
+    return (
+      <div>
+        <Login authService={this.authService} />
+        App from class
+      </div>
+    );
+  }
+}
---
+++ src/components/Login.tsx
@@ -0,0 +1,68 @@
+import React, { SyntheticEvent } from "react";
+import { AuthService } from "../services/AuthService";
+
+interface LoginProps {
+  authService: AuthService;
+}
+interface LoginState {
+  userName: string;
+  password: string;
+  loginAttempted: boolean;
+  loginSuccessful: boolean;
+}
+
+interface CustomEvent {
+  target: HTMLInputElement;
+}
+
+export class Login extends React.Component<LoginProps, LoginState> {
+  state: LoginState = {
+    userName: "",
+    password: "",
+    loginAttempted: false,
+    loginSuccessful: false,
+  };
+
+  private setUserName(event: CustomEvent) {
+    this.setState({ userName: event.target.value });
+  }
+
+  private setPassword(event: CustomEvent) {
+    this.setState({ password: event.target.value });
+  }
+
+  private async handleSubmit(event: SyntheticEvent) {
+    event.preventDefault();
+    const result = await this.props.authService.login(
+      this.state.userName,
+      this.state.password
+    );
+    if (result) {
+      console.log(result);
+    } else {
+      console.log("bad login");
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Please Login</h2>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <input
+            value={this.state.userName}
+            onChange={(e) => this.setUserName(e)}
+          />
+          <br />
+          <input
+            value={this.state.password}
+            onChange={(e) => this.setPassword(e)}
+            type="password"
+          />
+          <br />
+          <input type="submit" value="Login" />
+        </form>
+      </div>
+    );
+  }
+}
